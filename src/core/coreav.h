fd448f31f801358f7329828faf3bccda23e067e2
Zoff <zoff99@users.noreply.github.com>
2018-01-01 14:16:50 +0100

    VIDEO_DEFAULT_BITRATE = 8000

--- src/core/coreav.h
+++ src/core/coreav.h
@@ -111,7 +111,7 @@ private:
                                    int32_t ystride, int32_t ustride, int32_t vstride, void* self);
 
 private:
-    static constexpr uint32_t VIDEO_DEFAULT_BITRATE = 2500;
+    static constexpr uint32_t VIDEO_DEFAULT_BITRATE = 8000; // works good: 2500;
 
 private:
     ToxAV* toxav;
