82e020336df843eced6fa5f049c7d4dfec40e647
Zoff <zoff99@users.noreply.github.com>
2018-01-15 15:11:51 +0000

    remove debug info

--- windows/cross-compile/build.sh
+++ windows/cross-compile/build.sh
@@ -886,8 +886,6 @@ TOXCORE_PREFIX_DIR="$DEP_DIR/libtoxcore"
 TOXCORE_HASH=not_used
 TOXCORE_CUSTOM_VERSION=${TOXCORE_CUSTOM_VERSION:-}
 
-# env|sort
-
 if [ "$TOXCORE_CUSTOM_VERSION""x" == "x" ]; then
   TOXCORE_REPO="https://github.com/TokTok/c-toxcore"
   TOXCORE_VERSION="v0.1.11"
