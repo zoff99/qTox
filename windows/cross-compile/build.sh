05fb53f4712236b0811adaf558ac66ae51c8b93b
Zoff <zoff99@users.noreply.github.com>
2018-01-15 13:46:55 +0000

    env

--- windows/cross-compile/build.sh
+++ windows/cross-compile/build.sh
@@ -885,7 +885,11 @@ fi
 TOXCORE_PREFIX_DIR="$DEP_DIR/libtoxcore"
 TOXCORE_HASH=not_used
 
+env|sort|grep -i tox
+
 TOXCORE_CUSTOM_VERSION=${TOXCORE_CUSTOM_VERSION:-}
+
+env|sort|grep -i tox
 
 if [ "$TOXCORE_CUSTOM_VERSION""x" == "x" ]; then
   TOXCORE_REPO="https://github.com/TokTok/c-toxcore"
